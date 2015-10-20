cd009f9bf181ec7dd4c03302a50ba46752e2c788
Ryan Dignard <conceptuallyflawed@gmail.com>
2015-10-20 15:00:10 -0700

    found a bug, topClassDeclaringPropertyNamed checks the class implementation and does not compare canonically

--- RestGoatee-Core/NSObject+RG_Deserialization.m
+++ RestGoatee-Core/NSObject+RG_Deserialization.m
@@ -103,7 +103,7 @@ static NSArray* rg_unpackArray(NSArray* json, id context) {
 - (void) rg_initCanonically:(prefix_nonnull NSString*)key withValue:(prefix_nullable id)value inContext:(prefix_nullable id)context {
     NSUInteger index = [self.__property_list__[kRGPropertyCanonicalName] indexOfObject:key.canonicalValue];
     if (index != NSNotFound) {
-        if (topClassDeclaringPropertyNamed([self class], key.canonicalValue) != [NSObject class]) {
+        if (topClassDeclaringPropertyNamed([self class], self.__property_list__[index][kRGPropertyName]) != [NSObject class]) {
             @try {
                 [self rg_initProperty:self.__property_list__[index][kRGPropertyName] withValue:value inContext:context];
             } @catch (NSException* e) { /* Should this fail the property is left alone */

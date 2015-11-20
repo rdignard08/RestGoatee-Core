b4a37f4f60668ae9cd14e2afbdc19f7acf63a36a
Ryan Dignard <conceptuallyflawed@gmail.com>
2015-11-19 17:11:37 -0800

    dont want to test this

--- RestGoatee-Core-Tests/RGTestObject1.m
+++ RestGoatee-Core-Tests/RGTestObject1.m
@@ -1,11 +1,6 @@
 
 #import "RGTestObject1.h"
-#import "RestGoatee-Core.h"
 
 @implementation RGTestObject1
-
-- (NSString*) description {
-    return [NSString stringWithFormat:@"%@ %@", [super description], [self dictionaryRepresentation]];
-}
 
 @end

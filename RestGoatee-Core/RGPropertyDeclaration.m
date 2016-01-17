1ab8af6babe8efeea564b31d0b12e9257c5d9353
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-16 22:18:20 -0800

    using VLAs is twice as fast

--- RestGoatee-Core/RGPropertyDeclaration.m
+++ RestGoatee-Core/RGPropertyDeclaration.m
@@ -3,11 +3,10 @@
 
 RG_FILE_START
 
-NSString* const rg_canonical_form(const char* const utfName) {
-    const size_t nameLength = strlen(utfName);
+static NSString* RG_SUFFIX_NONNULL const rg_malloc_based_canonical(const char* RG_SUFFIX_NONNULL const utfName, size_t length) {
+    char* canonicalBuffer = malloc(length);
     size_t outputLength = 0;
-    char* canonicalBuffer = malloc(nameLength);
-    for (size_t i = 0; i != nameLength; i++) {
+    for (size_t i = 0; i != length; i++) {
         char c = utfName[i];
         if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) { /* a digit or lowercase character; no change */
             canonicalBuffer[outputLength++] = c;
@@ -16,6 +15,25 @@ NSString* const rg_canonical_form(const char* const utfName) {
         } /* unicodes, symbols, spaces, etc. are completely skipped */
     }
     return [[NSString alloc] initWithBytesNoCopy:canonicalBuffer length:outputLength encoding:NSUTF8StringEncoding freeWhenDone:YES];
+}
+
+static NSString* RG_SUFFIX_NONNULL const rg_static_based_canonical(const char* RG_SUFFIX_NONNULL const utfName, size_t length) {
+    char canonicalBuffer[length];
+    size_t outputLength = 0;
+    for (size_t i = 0; i != length; i++) {
+        char c = utfName[i];
+        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) { /* a digit or lowercase character; no change */
+            canonicalBuffer[outputLength++] = c;
+        } else if (c >= 'A' && c <= 'Z') { /* an uppercase character; to lower */
+            canonicalBuffer[outputLength++] = c + (const int)('a' - 'A'); /* 'a' - 'A' == 32 */
+        } /* unicodes, symbols, spaces, etc. are completely skipped */
+    }
+    return [[NSString alloc] initWithBytes:canonicalBuffer length:outputLength encoding:NSUTF8StringEncoding];
+}
+
+NSString* RG_SUFFIX_NONNULL const rg_canonical_form(const char* RG_SUFFIX_NONNULL const utfName) {
+    const size_t length = strlen(utfName);
+    return length >= 1024 || __STDC_NO_VLA__ ? rg_malloc_based_canonical(utfName, length) : rg_static_based_canonical(utfName, length);
 }
 
 @implementation RGPropertyDeclaration

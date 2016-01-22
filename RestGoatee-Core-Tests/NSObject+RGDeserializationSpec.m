170b58a8fbd2e510fe77bae5da42a5df24cf0df8
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-21 23:18:47 -0800

    missing test

--- RestGoatee-Core-Tests/NSObject+RGDeserializationSpec.m
+++ RestGoatee-Core-Tests/NSObject+RGDeserializationSpec.m
@@ -553,6 +553,13 @@ CATEGORY_SPEC(NSObject, RGDeserialization)
     XCTAssert([object.stringProperty isEqual:@"a string"]);
 }
 
+- (void) testEmptyNodeToString {
+    RGTestObject2* object = [RGTestObject2 new];
+    RGXMLNode* node = [[RGXMLNode alloc] initWithName:@""];
+    [object rg_initProperty:(id RG_SUFFIX_NONNULL)[RGTestObject2 rg_propertyList][RG_STRING_SEL(stringProperty)] withValue:node inContext:nil];
+    XCTAssert([object.stringProperty isEqual:@""]);
+}
+
 - (void) testNodeToURL {
     RGTestObject2* object = [RGTestObject2 new];
     RGXMLNode* node = [[RGXMLNode alloc] initWithName:@""];

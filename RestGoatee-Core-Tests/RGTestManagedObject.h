b08cc939fe251b305a034c5ddfb031ac75aa551b
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-01-21 18:17:25 -0800

    add test managed object to tests

---
+++ RestGoatee-Core-Tests/RGTestManagedObject.h
@@ -0,0 +1,9 @@
+
+#import <CoreData/CoreData.h>
+
+@interface RGTestManagedObject : NSManagedObject
+
+@property (nonatomic, strong) NSString* id;
+@property (nonatomic, strong) NSNumber* fooProperty;
+
+@end
---
+++ RestGoatee-Core-Tests/RGTestManagedObject.m
@@ -0,0 +1,8 @@
+
+#import "RGTestManagedObject.h"
+
+@implementation RGTestManagedObject
+@synthesize id = _id;
+@synthesize fooProperty = _fooProperty;
+
+@end

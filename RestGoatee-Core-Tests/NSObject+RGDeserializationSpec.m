7a6a35c03d4a655f2f88a1313ff2ca81ebd35fe8
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-02-08 21:46:31 -0800

    technically possible now

--- RestGoatee-Core-Tests/NSObject+RGDeserializationSpec.m
+++ RestGoatee-Core-Tests/NSObject+RGDeserializationSpec.m
@@ -525,6 +525,12 @@ CATEGORY_SPEC(NSObject, RGDeserialization)
     XCTAssert(object.dictionaryProperty == nil);
 }
 
+- (void) testNumberToDate {
+    RGTestObject2* object = [RGTestObject2 new];
+    [object rg_initProperty:(id RG_SUFFIX_NONNULL)[RGTestObject2 rg_propertyList][RG_STRING_SEL(dateProperty)] withValue:@12.12 inContext:nil];
+    XCTAssert(object.dateProperty == nil);
+}
+
 #pragma mark - rg_initProperty:withValue:inContext: with nil
 - (void) testNilToString {
     RGTestObject2* object = [RGTestObject2 new];

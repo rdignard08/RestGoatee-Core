f7d64bec0dcb50020adfbbc7afeddae783898788
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-03-05 01:42:10 -0800

    bare minimum script

--- oclint_run.sh
+++ oclint_run.sh
@@ -2,33 +2,24 @@ source ~/.bash_profile
  
 hash oclint &> /dev/null
 if [ $? -eq 1 ]; then
-echo >&2 "oclint not found, analyzing stopped"
-exit 1
+    echo >&2 "oclint not found, analyzing stopped"
+    exit 1
 fi
  
 cd ${TARGET_TEMP_DIR}
  
-    echo "[*] compile_commands.json not found, possibly clean was performed"
-    echo "[*] starting xcodebuild to rebuild the project.."
-    # clean previous output
-    if [ -f xcodebuild.log ]; then
-    rm xcodebuild.log
-    fi
+rm xcodebuild.log
  
-    cd ${SRCROOT}
+cd ${SRCROOT}
  
 xcodebuild -project RestGoatee-Core.xcodeproj -scheme RestGoatee-Core clean build | xcpretty -r json-compilation-database
-ls 
-    echo "[*] transforming xcodebuild.log into compile_commands.json..."
-    cd ${TARGET_TEMP_DIR}
-    #transform it into compile_commands.json
- 
-    echo "[*] copy compile_commands.json to the project root..."
-    cp ${TARGET_TEMP_DIR}/compile_commands.json ${SRCROOT}/compile_commands.json
- 
- 
-echo "[*] starting analyzing"
+
 cd ${TARGET_TEMP_DIR}
+ 
+cp ${TARGET_TEMP_DIR}/compile_commands.json ${SRCROOT}/compile_commands.json
+ 
+cd ${TARGET_TEMP_DIR}
+
 oclint-json-compilation-database | sed 's/\(.*\.\m\{1,2\}:[0-9]*:[0-9]*:\)/\1 warning:/'
 
 printf '\7\7' # notify user that the task is done

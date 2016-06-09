48f604d38cacc0ed69d4bc6ee35a7a25170ac2a8
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-06-09 12:50:36 -0700

    Update RGPropertyDeclarationSpec.m

--- RestGoatee-Core-Tests/RGPropertyDeclarationSpec.m
+++ RestGoatee-Core-Tests/RGPropertyDeclarationSpec.m
@@ -60,7 +60,7 @@ CLASS_SPEC(RGPropertyDeclaration)
     XCTAssert(stringProperty.isPrimitive == NO);
     XCTAssert(stringProperty.isReadOnly == NO);
     XCTAssert(stringProperty.storageSemantics == kRGPropertyStrong);
-    XCTAssert(stringProperty.backingIvar == nil);
+    XCTAssert([stringProperty.backingIvar isEqual:@"stringProperty"]);
     XCTAssert(stringProperty.isDynamic == NO);
     XCTAssert(stringProperty.isAtomic == NO);
     RGPropertyDeclaration* numberProperty = properties[RG_STRING_SEL(intProperty)];

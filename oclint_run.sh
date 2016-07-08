c3d22f73e44e680b4a7087ac57051828ba0910b1
Ryan Dignard <conceptuallyflawed@gmail.com>
2016-07-07 21:30:36 -0700

    fixup oclint_run.sh

--- oclint_run.sh
+++ oclint_run.sh
@@ -5,18 +5,12 @@ if [ $? -eq 1 ]; then
     echo >&2 "oclint not found, analyzing stopped"
     exit 1
 fi
- 
-rm -f ${TARGET_TEMP_DIR}/xcodebuild.log
- 
-cd ${SRCROOT}
- 
-xcodebuild -project RestGoatee-Core.xcodeproj -scheme RestGoatee-Core clean build | xcpretty -r json-compilation-database
 
-cd ${TARGET_TEMP_DIR}
- 
-cp ${TARGET_TEMP_DIR}/compile_commands.json ${SRCROOT}/compile_commands.json
- 
-cd ${TARGET_TEMP_DIR}
+if [ ! -z "${SRCROOT}" ]; then 
+    cd ${SRCROOT}
+fi
+
+xcodebuild -verbose -project RestGoatee-Core.xcodeproj -scheme RestGoateeCoreiOS -sdk iphonesimulator9.2 -destination "platform=iOS simulator,name=iPhone 5" clean build | xcpretty -r json-compilation-database -o ./compile_commands.json
 
 oclint-json-compilation-database | sed 's/\(.*\.\m\{1,2\}:[0-9]*:[0-9]*:\)/\1 warning:/'
 
